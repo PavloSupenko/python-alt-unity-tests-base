19cbb71b2ac526689160999ba8567837b938412f
Pavlo Supenko <pavlo.supenko@bini.games>
2022-10-19 11:08:11 +0300

    Add names to tests artifact directories;

--- tests/base/appium_screenshot.py
+++ tests/base/appium_screenshot.py
@@ -4,13 +4,12 @@ from appium.webdriver.webdriver import WebDriver
 
 class AppiumScreenshot:
     def __init__(self, screenshotName):
-        current_test_number = os.environ["CUSTOM_TEST_NUMBER"]
-        artifacts_directory = os.environ['DEVICEFARM_LOG_DIR']
-        self.screenshot_path = os.path.join(artifacts_directory, 'tests', f"{current_test_number}", f"{screenshotName}.png")
-        screenshot_directory = os.path.dirname(self.screenshot_path)
+        test_artifacts_path = os.environ["CUSTOM_CURRENT_TEST_DIR"]
+        self.screenshot_path = os.path.join(test_artifacts_path, f"{screenshotName}.png")
 
         # Create subdirectories if it's needed based on filename
         # It's pretty important to pass directory path as the argument, not file path
+        screenshot_directory = os.path.dirname(self.screenshot_path)
         os.makedirs(screenshot_directory, exist_ok=True)
         print(f"Screenshots save directory: {self.screenshot_path}")
 
